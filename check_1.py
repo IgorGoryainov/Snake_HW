871e4923fcee5a21310b7ca9ceb3ddd7c51305e2
IgorGoryainov <likeiigor@gmail.com>
2022-10-24 19:57:43 +0300

    Add main classes

--- check_1.py
+++ check_1.py
@@ -1,45 +1,60 @@
 import pygame
 
-WIDTH = 360
-HEIGHT = 480
-FPS = 30
+running = True
 
-pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Snake")
-clock = pygame.time.Clock()
+all_sprites = pygame.sprite.Group()
+
+class GameProcess:
+    def __init__(self):
+        self.WIDTH = 360
+        self.HEIGHT = 480
+        self.FPS = 15
+        self.clock = pygame.time.Clock()
+
+    def init_game(self):
+        pygame.init()
+
+    def set_surface(self):
+        self.screen = pygame.display.set_mode((self.WIDTH, self.HEIGHT))
+        pygame.display.set_caption("Snake")
+        self.screen.fill((204, 255, 255))
+
+    def get_events(self):
+        global running
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+    def update_screen(self):
+        global all_sprites
+        pygame.display.flip()
+        self.clock.tick(self.FPS)
+        all_sprites.draw(self.screen)
+
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface((100, 25))
+        self.image = pygame.Surface((25, 25))
         self.image.fill((255, 51, 102))
         self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH / 2, HEIGHT / 2)
+        self.rect.center = (100, 100)
 
     def update(self):
         self.rect.x += 5
-        if self.rect.left > WIDTH:
-            self.rect.right = 0
 
-all_sprites = pygame.sprite.Group()
+
+process = GameProcess()
 player = Player()
 all_sprites.add(player)
 
-running = True
+process.init_game()
+process.set_surface()
+
 while running:
-    clock.tick(FPS)
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    all_sprites.update()
-
-    screen.fill((204, 255, 255))
-    all_sprites.draw(screen)
-
-    pygame.display.flip()
+    player.update()
+    process.get_events()
+    process.update_screen()
 
 
 pygame.quit()
